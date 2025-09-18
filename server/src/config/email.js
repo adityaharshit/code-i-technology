52d98740b34cd1e3d73f700c8052d5b054eb4e4d
Harshit Aditya <goluandharshit@gmail.com>
2025-09-18 14:02:07 +0530

    hoping to fix mailing issues

--- server/src/config/email.js
+++ server/src/config/email.js
@@ -3,9 +3,9 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST, // e.g., 'smtp.gmail.com' for Gmail
+  host: process.env.SMTP_HOST, 
   port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT) : 587,
-  secure: false, // `true` for port 465, `false` for all other ports
+  secure: false, 
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
