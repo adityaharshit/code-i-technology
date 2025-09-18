a9c0dc934221664caf7494142738a7d6817e77ea
Harshit Aditya <goluandharshit@gmail.com>
2025-09-18 12:52:26 +0530

    trying to fix mailing issues

--- server/src/config/email.js
+++ server/src/config/email.js
@@ -5,22 +5,22 @@ require('dotenv').config();
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST, // e.g., 'smtp.gmail.com' for Gmail
   port: process.env.SMTP_PORT ? parseInt(process.env.SMTP_PORT) : 587,
-  secure: process.env.SMTP_PORT === '465', // `true` for port 465, `false` for all other ports
+  secure: false, // `true` for port 465, `false` for all other ports
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   }
 });
 
-transporter
-  .verify()
-  .then(() => console.log('Email transporter is ready and verified.'))
-  .catch((err) =>
-    console.error(
-      'Failed to verify email transporter. Check your .env file for correct SMTP credentials and ensure your hosting provider allows outbound connections on the specified port.',
-      err,
-    ),
-  );
+// transporter
+//   .verify()
+//   .then(() => console.log('Email transporter is ready and verified.'))
+//   .catch((err) =>
+//     console.error(
+//       'Failed to verify email transporter. Check your .env file for correct SMTP credentials and ensure your hosting provider allows outbound connections on the specified port.',
+//       err,
+//     ),
+//   );
 
 const sendEmail = async (to, subject, html, replyTo = null) => {
   try {
